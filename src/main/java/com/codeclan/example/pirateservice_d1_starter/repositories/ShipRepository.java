ac1ab61641cdd0281dc4fd02d2acfeba15d21dfe
joedillonuk <joe.dillon.uk@gmail.com>
2020-07-27 15:43:11 +0100

    One to many lesson completed.

---
+++ src/main/java/com/codeclan/example/pirateservice_d1_starter/repositories/ShipRepository.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.pirateservice_d1_starter.repositories;
+
+import com.codeclan.example.pirateservice_d1_starter.models.Ship;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ShipRepository extends JpaRepository<Ship, Long> {
+}
